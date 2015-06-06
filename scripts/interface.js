b796119cb0353df3528c4a6dac1f633f41c0d0c9
gabriel-hook <gabriel.hook@gmail.com>
2015-06-05 18:36:09 -0700

    dont swipe when popup overlay is on

--- scripts/interface.js
+++ scripts/interface.js
@@ -294,10 +294,13 @@ $( document ).on( "pagecreate", "#page-add", function() {
 //Make swipes move to the next screen
 $( document ).on( "pagecreate", "#page-roster", function() {
   $( document ).on( "swipeleft", "#page-roster", function( e ) {
+    if($("#page-roster").find(".ui-popup-active").length)
+      return;
     $("#page-roster").find("#footer a[href=#page-teams]").click()
   });
   $( document ).on( "swiperight", "#page-roster", function( e ) {
-
+    if($("#page-roster").find(".ui-popup-active").length)
+      return;
     if($("#page-roster").find(".panel").hasClass("ui-panel-open"))
       $("#page-roster").find(".panel a[href=#page-add]").click()
     else
