54808c924f8d04a9686bcac2810bce562ff71d22
gabriel-hook <gabriel.hook@gmail.com>
2015-06-05 18:32:30 -0700

    cleanup

--- scripts/onboarding.js
+++ scripts/onboarding.js
@@ -1,23 +1,17 @@
 ﻿$("#page-roster").on( "pageshow", function() {
   if(CoC.ui.roster.empty){
-  
     $("#onboarding-roster").addClass("show")
     $("#page-roster").one("click",function(){
-    
       $("#onboarding-roster").removeClass("show")
     })
-    
   }
 });
 
 $("#page-teams").on( "pageshow", function() {
   if(CoC.ui.teams.empty){
-  
     $("#onboarding-teams").addClass("show")
     $("#page-teams").one("click",function(){
-    
       $("#onboarding-teams").removeClass("show")
     })
-  
   }
 });
