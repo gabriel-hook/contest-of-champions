eac407f078118405fb65fcb4dd733c4274eee8d9
gabriel-hook <gabriel.hook@gmail.com>
2015-07-07 09:31:25 -0700

    set up as separate function adds

--- scripts/coc-guides.js
+++ scripts/coc-guides.js
@@ -1,65 +1,60 @@
-var CoC = CoC || {};
+var CoC=CoC || {};
+CoC.guides=CoC.guides || {}
 
-CoC.guides = new function(){
+CoC.guides.initialize=function(){
+  CoC.guides.data = {};
+  setTimeout(function(){
   
-  this.data = {}
-
-  this.initialize=function(){
-    var that = this;
-    setTimeout(function(){
-    
-      var uids = _.uniq( CoC.data.champions.pluck("uid") );
-      var working = uids.length;
-      _(uids).each(function(uid){
-        $.ajax({
-          url: "scripts/data/guides/"+uid+".json",
-          complete:function(response){
-            var data;
-            try{
-              if(response.status === 200)
-                data = JSON.parse( response.responseText );
-            }
-            catch(error){
-              console.error(error);
-            }
-            CoC.guides.add(uid, data);
-            
-            //if done all of them, do callback
-            working--;
-            if(!working){
-              that._completed = true;
-              that._onComplete();
-            }
+    var uids = _.uniq( CoC.data.champions.pluck("uid") );
+    var working = uids.length;
+    _(uids).each(function(uid){
+      $.ajax({
+        url: "scripts/data/guides/"+uid+".json",
+        complete:function(response){
+          var data;
+          try{
+            if(response.status === 200)
+              data = JSON.parse( response.responseText );
           }
-        });
+          catch(error){
+            console.error(error);
+          }
+          CoC.guides.add(uid, data);
+          
+          //if done all of them, do callback
+          working--;
+          if(!working){
+            CoC.guides._completed = true;
+            if(CoC.guides._onComplete !== undefined)
+              CoC.guides._onComplete();
+          }
+        }
       });
-      
-    },0);
+    });
+    
+  },0);
+}
+  
+CoC.guides.get=function(uid){
+  return CoC.guides.data[uid];
+}
+  
+CoC.guides.add=function(uid, data){
+  var champion = CoC.data.champions.findWhere({ uid:uid }).clone();
+  champion.set("stars", 0);
+  var value = {
+    uid:uid,
+    champion:champion
   };
-  
-  this.get=function(uid){
-    return CoC.guides.data[uid];
-  }
-  
-  this.add=function(uid, data){
-    var champion = CoC.data.champions.findWhere({ uid:uid }).clone();
-    champion.set("stars", 0);
-    var value = {
-      uid:uid,
-      champion:champion
-    };
-    if(data)
-      value.data = data;
-    CoC.guides.data[uid] = value;
-  }
+  if(data)
+    value.data = data;
+  CoC.guides.data[uid] = value;
+}
 
-  this.complete=function(func){
-    if(this._completed){
-      func();
-      return;
-    }
-    this._onComplete = func;
+CoC.guides.complete=function(func){
+  if(CoC.guides._completed){
+    func();
+    return;
   }
-  this._onComplete = function(){}
-  this._completed = false;
+  CoC.guides._onComplete = func;
 }
