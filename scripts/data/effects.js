c4f7bb005bc3de1bf0854c465a965046d3a67fb2
gabriel-hook <gabriel.hook@gmail.com>
2015-07-22 12:23:27 -0700

    moved stun up next to attack

--- scripts/data/effects.js
+++ scripts/data/effects.js
@@ -5,9 +5,9 @@ CoC.data = CoC.data || {};
 CoC.data.effects = new Backbone.Collection([
 
   { uid: "attack", name:"Attack", image:"images/effects/attack.jpg", base:5 },
+  { uid: "stun", name:"Stun", image:"images/effects/stun.jpg", base:15 },
   { uid: "critrate", name:"Critical Rate", image:"images/effects/critical.jpg", base:5 },
   { uid: "critdamage", name:"Critical Damage", image:"images/effects/critical.jpg", base:15 },
-  { uid: "stun", name:"Stun", image:"images/effects/stun.jpg", base:15 },
   { uid: "powergain", name:"Power Gain", image:"images/effects/mana.jpg", base:3 },
   { uid: "powersteal", name:"Power Steal", image:"images/effects/mana_steal.jpg", base:3 },
   { uid: "perfectblock", name:"Perfect Block", image:"images/effects/block.jpg", base:3 },
