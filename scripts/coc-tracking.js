3a46e74b69638ff76c7afdd98be09d447f43e6ce
gabriel-hook <gabriel.hook@gmail.com>
2015-07-31 13:02:24 -0700

    Revert "added whitespace for cleaner formatting"
    This reverts commit 6231b15caa1cddc0670c324b825a0154349c0f73.

--- scripts/coc-tracking.js
+++ scripts/coc-tracking.js
@@ -1,24 +1,17 @@
 ﻿var CoC=CoC || {};
 CoC.tracking = CoC.tracking || {};
 
-//Google Analytics Init: make GA and create
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','_gaTracker');
-_gaTracker('create', 'UA-64735733-1', 'auto');
-
 CoC.tracking.pageView = function(){
-  var array = ['send', 'pageview'];
+  var array = ['_trackPageview'];
   for(var i=0; i<arguments.length; i++)
     array.push(arguments[i]);
-  _gaTracker.apply(_gaTracker, array);
-};
-
+  _gaq.push(array);
+}
 CoC.tracking.event = function(){
-  var array = ['send','event'];
+  var array = ['_trackEvent'];
   for(var i=0; i<arguments.length; i++)
     array.push(arguments[i])
-  _gaTracker.apply(_gaTracker, array);
-};
+  _gaq.push(array);
+}
 
+_gaq.push(['_setAccount', 'UA-64735733-1']);
