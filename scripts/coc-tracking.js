577347b357d1168f03a00f1a1485497b348371fb
gabriel-hook <gabriel.hook@gmail.com>
2015-07-31 13:03:16 -0700

    use new analytics and embed entirely in tracking js

--- scripts/coc-tracking.js
+++ scripts/coc-tracking.js
@@ -1,17 +1,23 @@
 ﻿var CoC=CoC || {};
 CoC.tracking = CoC.tracking || {};
 
+//Google Analytics Init: make GA and create
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','_gaTracker');
+_gaTracker('create', 'UA-64735733-1', 'auto');
+
 CoC.tracking.pageView = function(){
-  var array = ['_trackPageview'];
+  var array = ['send', 'pageview'];
   for(var i=0; i<arguments.length; i++)
     array.push(arguments[i]);
-  _gaq.push(array);
-}
+  _gaTracker.apply(_gaTracker, array);
+};
+
 CoC.tracking.event = function(){
-  var array = ['_trackEvent'];
+  var array = ['send','event'];
   for(var i=0; i<arguments.length; i++)
     array.push(arguments[i])
-  _gaq.push(array);
-}
-
-_gaq.push(['_setAccount', 'UA-64735733-1']);
+  _gaTracker.apply(_gaTracker, array);
+};
