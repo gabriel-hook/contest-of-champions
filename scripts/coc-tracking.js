71206a294d92f374674744853bce6026f47cf841
gabriel-hook <gabriel.hook@gmail.com>
2015-08-01 13:57:46 -0700

    add some basic exception tracking

--- scripts/coc-tracking.js
+++ scripts/coc-tracking.js
@@ -21,3 +21,15 @@ CoC.tracking.event = function(){
     array.push(arguments[i])
   _gaTracker.apply(_gaTracker, array);
 };
+
+window.onerror = function(description, file, line){
+  //Send (description text...\t filename:line) exception message for size 155 bytes
+  var location = "\t"+((file.lastIndexOf('/') !== -1)? file.substr(file.lastIndexOf('/')+1): file)+":"+line,
+    bytes = 155 - unescape(encodeURIComponent(location)).length;
+  while(unescape(encodeURIComponent(description)).length > bytes)
+    description = description.substr(0, description.length - 1);
+  _gaTracker('send', 'exception', {
+    'exDescription': description + location,
+    'exFatal': true
+  });
+};
