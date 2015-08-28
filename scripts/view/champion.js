daaee0fa926abf74b1558458a8a74a02ff8a9df0
gabriel-hook <gabriel.hook@gmail.com>
2015-08-28 00:39:20 -0700

    dont use placeholder if we already loaded portrait image

--- scripts/view/champion.js
+++ scripts/view/champion.js
@@ -1,32 +1,46 @@
 var CoC = CoC || {};
 CoC.view = CoC.view || {};
-CoC.view.ChampionView = Backbone.View.extend({
-  tagName: 'div',
-  template: _.template( $('#championTemplate').html() ),
-  render:function(){
-    var model = this.model,
-      json = model.toJSON();
-    json.portrait = model.portrait();
 
+(function(){
+  var portraitImages = {};
 
-    var el = this.$el;
-    //set my contents, classes and attributes
-    el.html( this.template(json) );
-    el.addClass("champion").addClass(model.get("typeId"));
-    el.attr("uid", model.get("uid")).attr("stars", model.get("stars"));
+  CoC.view.ChampionView = Backbone.View.extend({
+    tagName: 'div',
+    template: _.template( $('#championTemplate').html() ),
+    render:function(){
+      var model = this.model,
+        json = model.toJSON();
+      json.portrait = model.portrait();
 
-    //show and animate placeholder if we take a while to load
-    setTimeout(function(){
-      el.find(".portrait").addClass('fade');
-      el.find(".portrait-placeholder").addClass('fade');
-    }, 250);
+      var el = this.$el;
+      el.html( this.template(json) );
 
-    return this;
+      //set classes and attributes
+      el.addClass("champion").addClass(model.get("typeId"));
+      el.attr("uid", model.get("uid")).attr("stars", model.get("stars"));
+
+      //init portrait placeholder swap, skip if we have the image already
+      var image = el.find(".portrait"), placeholder = el.find(".portrait-placeholder");
+      if(portraitImages[image[0].src]){
+        image.addClass('loaded');
+        placeholder.addClass('loaded');
+      }
+      else{
+        image.addClass('fade');
+        placeholder.addClass('fade');
+      }
+      return this;
+    }
+  });
+
+  CoC.view.championPortraitLoaded=function(img){
+    portraitImages[img.src] = true;
+    
+    var parent = $(img.parentNode), 
+      image = parent.find('.portrait'), 
+      placeholder = parent.find(".portrait-placeholder");
+
+    image.addClass('loaded');
+    placeholder.addClass('loaded');
   }
-});
-
-CoC.view.championPortraitLoaded=function(parentNode){
-  var parent = $(parentNode);
-  parent.find('.portrait').addClass('loaded');
-  parent.find(".portrait-placeholder").addClass('loaded');
-}
+})();
