968385f090c5964845babafcc9e255938ac6d826
gabriel-hook <gabriel.hook@gmail.com>
2015-09-19 10:44:10 -0700

    use npm for packages and gulp to build

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var gulp = require('gulp');
+var rename = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', function() {
+  
+  console.log("gulping");
+
+});
