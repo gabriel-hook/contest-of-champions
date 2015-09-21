1561e914b089a1d807bee09be5b59e6c9dfbe051
gabriel-hook <gabriel.hook@gmail.com>
2015-09-21 00:31:06 -0700

    clean up code and move json namespacing code to named function

--- gulpfile.js
+++ gulpfile.js
@@ -26,31 +26,18 @@ gulp.task('clean:css', function(){
 gulp.task('build', ['build:js', 'build:css']);
 gulp.task('build:js', function(){
   eachScript(function(name, files){
+    //regular muliple javascript minification
     gulp.src(files)
         .pipe(sourcemaps.init())
-          .pipe(concat(name+'.min.js'))
+          .pipe(concat(name + '.min.js'))
           .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
+        .pipe(sourcemaps.write('.', { includeContent:false }))
         .pipe(gulp.dest('./js'));
   },function(name, json, files){
+    //combine multiple json files into a namespaced 
     gulp.src(files)
         .pipe(jsoncombine(name+'.min.js', function(data){
-          var i = 0;
-          var string = '';
-          var namespace = json.split('.');
-          var currentNamespace = namespace[0];
-          if(namespace.length === 1){
-            string = 'var '+currentNamespace+' = '+ JSON.stringify(data) + ";\n";
-          }
-          else{
-            string = 'var '+currentNamespace+' = '+currentNamespace+" || {};\n";
-            for(i = 1; i < namespace.length - 1; i++){
-              currentNamespace += '.' + namespace[i];
-              string += currentNamespace+' = '+currentNamespace+" || {};\n";
-            }
-            currentNamespace += '.' + namespace[i]
-            string += currentNamespace+' = '+JSON.stringify(data) + ";\n";
-          }
+          var string = namespaceJSON(json, data)
           return new Buffer(string, 'utf-8');
         }))
         .pipe(uglify())
@@ -59,13 +46,15 @@ gulp.task('build:js', function(){
 });
 gulp.task('build:css', function(){
   eachStyle(function(name, files){
+    //regular multiple css minification
     gulp.src(files)
         .pipe(sourcemaps.init())
-          .pipe(concat(name+'.min.css'))
+          .pipe(concat(name + '.min.css'))
           .pipe(minifyCss())
-        .pipe(sourcemaps.write('.'))
+        .pipe(sourcemaps.write('.', { includeContent:false }))
         .pipe(gulp.dest('./css'));
   });
+  //copy all fonts over
   gulp
     .src('./styles/fonts/*')
     .pipe(gulp.dest('./css/fonts'))
@@ -85,8 +74,29 @@ function eachScript(callback, jsonCallback){
       callback.call(null, scripts[i].name, scripts[i].files);
   }
 }
+
 function eachStyle(callback){
   for(var i=0; i<styles.length; i++){
     callback.call(null, styles[i].name, styles[i].files);
   }
+}
+
+function namespaceJSON(json, data){
+  var i = 0;
+  var string = '';
+  var namespace = json.split('.');
+  var currentNamespace = namespace[0];
+  if(namespace.length === 1){
+    string = 'var '+currentNamespace+' = '+ JSON.stringify(data) + ";\n";
+  }
+  else{
+    string = 'var '+currentNamespace+' = '+currentNamespace+" || {};\n";
+    for(i = 1; i < namespace.length - 1; i++){
+      currentNamespace += '.' + namespace[i];
+      string += currentNamespace+' = '+currentNamespace+" || {};\n";
+    }
+    currentNamespace += '.' + namespace[i]
+    string += currentNamespace+' = '+JSON.stringify(data) + ";\n";
+  }
+  return string;
 }
