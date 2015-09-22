2ca6b6fb6f60cf1d0d8f1b33537493a43b5cd03f
gabriel-hook <gabriel.hook@gmail.com>
2015-09-21 23:31:11 -0700

    better json combine and added template precompile

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var fs = require('fs');
 var gulp = require('gulp');
 var rename = require("gulp-rename");
 var rimraf = require('gulp-rimraf');
-var jsoncombine = require('gulp-jsoncombine');
+var wrap = require('gulp-wrap');
+var declare = require('gulp-declare');
+var jst = require('gulp-jst');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
@@ -26,7 +28,7 @@ gulp.task('clean:css', function(){
 
 gulp.task('build', ['build:js', 'build:css']);
 gulp.task('build:js', function(complete){
-  processScripts(function(name, files, done){
+  processScripts(function processScript(name, files, done){
     //regular muliple javascript minification
     gulp.src(files, { base: './' })
         .pipe(rename(excludeNpmPaths))    
@@ -36,19 +38,37 @@ gulp.task('build:js', function(complete){
         .pipe(sourcemaps.write('.', { includeContent:true }))
         .pipe(gulp.dest('./js'))
         .on('end', done);
-  },
-  function(name, json, files, done){
-    //combine multiple json files into a namespaced 
-    gulp.src(files)
-        .pipe(jsoncombine(name+'.min.js', function(data){
-          var string = namespaceJSON(json, data)
-          return new Buffer(string, 'utf-8');
+  }, function processJSON(name, json, files, done){
+    //combine multiple json files into namespaced definitions
+    gulp.src(files, { base: './' })
+        .pipe(sourcemaps.init())
+        .pipe(declare({
+          root: 'window',
+          namespace: json,
+          noRedeclare: true
         }))
+        .pipe(concat(name + '.min.js'))
         .pipe(uglify())
+        .pipe(sourcemaps.write('.', { includeContent:true }))
         .pipe(gulp.dest('./js'))
         .on('end', done);
-  },
-  complete);
+  }, function processTemplate(name, template, files, done){
+    //combine multiple html files into namespaced compiled templates
+    gulp.src(files, { base: './' })
+        .pipe(sourcemaps.init())
+        .pipe(jst())
+        .pipe(declare({
+          root: 'window',
+          namespace: template,
+          noRedeclare: true
+        }))
+        .pipe(concat(name + '.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.', { includeContent:true }))
+        .pipe(gulp.dest('./js'))
+        .on('end', done);
+
+  }, complete);
 });
 gulp.task('build:css', function(complete){
   processStyles(function(name, files, done){
@@ -76,11 +96,13 @@ gulp.task('watch', function(){
     gulp.watch('./styles/*', ['build:css']);
 });
 
-function processScripts(scriptCallback, jsonCallback, complete){
+function processScripts(scriptCallback, jsonCallback, templateCallback, complete){
   var done = doneCallback(complete);
   for(var i=0; i<scripts.length; i++){
     if(scripts[i].json)
       jsonCallback.call(null, scripts[i].name, scripts[i].json, scripts[i].files, done);
+    else if(scripts[i].template)
+      templateCallback.call(null, scripts[i].name, scripts[i].template, scripts[i].files, done);
     else
       scriptCallback.call(null, scripts[i].name, scripts[i].files, done);
   }
@@ -107,24 +129,4 @@ function excludeNpmPaths(path){
   if(path.dirname.indexOf('node_modules') === 0){
     path.dirname = 'scripts/lib/';
   }
-}
-
-function namespaceJSON(json, data){
-  var i = 0;
-  var string = '';
-  var namespace = json.split('.');
-  var currentNamespace = namespace[0];
-  if(namespace.length === 1){
-    string = 'var '+currentNamespace+' = '+ JSON.stringify(data) + ";\n";
-  }
-  else{
-    string = 'var '+currentNamespace+' = '+currentNamespace+" || {};\n";
-    for(i = 1; i < namespace.length - 1; i++){
-      currentNamespace += '.' + namespace[i];
-      string += currentNamespace+' = '+currentNamespace+" || {};\n";
-    }
-    currentNamespace += '.' + namespace[i];
-    string += currentNamespace+' = '+JSON.stringify(data) + ";\n";
-  }
-  return string;
 }
