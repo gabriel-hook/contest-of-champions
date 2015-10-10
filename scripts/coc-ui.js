12be99a5888388aaf799f871ffc9e2a36a412f4e
gabriel-hook <gabriel.hook@gmail.com>
2015-10-10 01:39:20 -0700

    refresh our worker link each time

--- scripts/coc-ui.js
+++ scripts/coc-ui.js
@@ -1,7 +1,7 @@
 var CoC = CoC || {};
 CoC.ui = CoC.ui || {};
 
-CoC.ui.workerScriptUrl = 'build/worker-teams.js?1';
+CoC.ui.workerScriptUrl = 'build/worker-teams.js?' + parseInt(new Date().getTime() % 1000000).toString(16);
 
 CoC.ui.initialize=function(){
   $("#popup-share").css('display','block').enhanceWithin().popup();
