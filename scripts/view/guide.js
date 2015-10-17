7bf89032bd5b015b1b404883c3f9a50350dbf8fa
gabriel-hook <gabriel.hook@gmail.com>
2015-10-17 16:28:18 -0700

    masonry cascading grid for guide

--- scripts/view/guide.js
+++ scripts/view/guide.js
@@ -173,8 +173,14 @@ CoC.view.GuideView = Backbone.View.extend({
   guideTemplate: CoC.data.template.guide,
   
   initialize:function(){
+    var that = this;
+
     this._guides = {};
     this._guideChampionsView = this.model;
+
+    $(window).bind("resize", function(){
+      that.boxLayout.call(that);
+    });
   },
   
   events:{
@@ -207,6 +213,29 @@ CoC.view.GuideView = Backbone.View.extend({
     $("#popup-guide-effect .ui-content").text(effect.get("description"));
     $("#popup-guide-effect").popup("open",{ positionTo:effectElement });
   },
+
+  boxLayout:function(){
+    var container = this.$el && this.$el.find('.boxes');
+    if(!container.is(':visible'))
+      return;
+
+    var box = container.find('.box');
+    var width = container.width();
+    var columnWidth = box.width();
+
+    var columns = Math.floor(width / columnWidth);
+    var gutter = (columns > 1)? (width - columnWidth * columns) / (columns - 1): 0;
+
+    console.log(columns, columnWidth, gutter);
+
+    container.masonry({
+      itemSelector: '.box',
+      columnWidth: columnWidth,
+      gutter: gutter,
+      isResizeBound: false,
+      transitionDuration: 0
+    });
+  },
   
   render:function(uid, forceRefresh){
     if(uid === undefined)
@@ -227,6 +256,8 @@ CoC.view.GuideView = Backbone.View.extend({
     this.$el.append( html );
     this.$el.trigger("create");
     this.$el.removeClass("dirty");
+
+    setTimeout(this.boxLayout.bind(this), 50);
     
     return this;
   }
