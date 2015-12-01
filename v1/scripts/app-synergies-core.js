70f29f9964549908a9b1409a3f4662e538af822e
gabriel-hook <gabrielhook@gmail.com>
2015-11-30 20:43:20 -0800

    removed v1

--- v1/scripts/app-synergies-core.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  var html = CoC.data.template['app-synergies']({
-    model: CoC.lang.model,
-    string: CoC.lang.string,
-    search: CoC.lang.search
-  });
-  $(document.body).append(html);
-})();
