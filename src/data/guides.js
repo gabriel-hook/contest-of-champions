dc5c81aba1d360fe3ad0c9b3cc26e059398afc0c
hook <gabrielhook@gmail.com>
2015-12-14 18:11:02 -0800

    added some spotlights

--- src/data/guides.js
+++ src/data/guides.js
@@ -1,19 +1,23 @@
 import blackbolt from './guide/blackbolt.json';
 import elektra from './guide/elektra.json';
+import guillotine from './guide/guillotine.json';
 import spidermanblack from './guide/spidermanblack.json';
 import thorjanefoster from './guide/thorjanefoster.json';
 import thevision from './guide/thevision.json';
 import ultron from './guide/ultron.json';
 import vision from './guide/vision.json';
+import warmachine from './guide/warmachine.json';
 
 const guides = {
     blackbolt,
     elektra,
+    guillotine,
     spidermanblack,
     thorjanefoster,
     thevision,
     ultron,
     vision,
+    warmachine,
 };
 
 export default guides;
