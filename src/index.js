8da12c2ba91a3584fcd2b1b7b58da1d369a5338d
hook <gabrielhook@gmail.com>
2015-12-23 17:58:10 -0800

    removed snow, its fun but bad for mobile experience

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { requestRedraw } from './util/animation';
 import './index.jsx';
-import { letItSnow } from './xmas/snow';
 import PACKAGE from '../package.json';
 
 /* eslint-disable no-console */
@@ -9,5 +8,3 @@ console.info(`${ PACKAGE.description } v${ PACKAGE.version }`);
 
 document.addEventListener('hotreload', () => requestRedraw(), true);
 window.addEventListener('resize', () => requestRedraw(10), true);
-
-letItSnow();
