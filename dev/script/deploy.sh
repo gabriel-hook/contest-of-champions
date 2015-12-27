aa6de0d9c7adf9a45c06d34b262b856796797f25
hook <gabrielhook@gmail.com>
2015-12-27 10:20:55 -0800

    proper setup to deploy to gh-pages

--- dev/script/deploy.sh
+++ dev/script/deploy.sh
@@ -6,5 +6,7 @@ git config user.name "Travis CI"
 git config user.email "hook@travis-ci.org"
 
 git add .
-git commit -m "Travis-CI auto-deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git commit --quiet -m "Travis-CI to GitHub Pages"
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1 || true
+
+echo "Deployed to gh-pages"
