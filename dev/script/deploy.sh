d08418f3ed16aea3d392d87395c5fc818ca39d3a
hook <gabrielhook@gmail.com>
2015-12-28 17:25:36 -0800

    better travis console message/order

--- dev/script/deploy.sh
+++ dev/script/deploy.sh
@@ -7,5 +7,6 @@ git config user.email "hook@travis-ci.org"
 
 git add .
 git commit --quiet -m "Travis-CI to GitHub Pages"
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-echo "Deployed to gh-pages"
+
+echo "Deploying to gh-pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
