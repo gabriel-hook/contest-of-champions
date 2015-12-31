b61ddfcdeb323cadd5010ca536806e9741e5af31
hook <gabrielhook@gmail.com>
2015-12-30 17:19:46 -0800

    remove unused function

--- src/util/notification.js
+++ src/util/notification.js
@@ -5,21 +5,6 @@ let denotify = function() { };
 
 const notifications = {};
 
-function isNewNotificationSupported() {
-    if (!window.Notification || !Notification.requestPermission)
-        return false;
-    if (Notification.permission === 'granted')
-        throw new Error('You must only call this *before* calling Notification.requestPermission(), otherwise this feature detect would bug the user with an actual notification!');
-    try {
-        new Notification('');
-    }
-    catch (e) {
-        if (e.name === 'TypeError')
-            return false;
-    }
-    return true;
-}
-
 if('Notification' in window) {
 
     if (window.Notification && Notification.permission !== 'granted') {
