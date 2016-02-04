33b8ce048340c16d5da86151f533b3dabb75130a
hook <gabrielhook@gmail.com>
2016-02-04 08:02:24 -0800

    proper object assignment spacing

--- src/data/types.js
+++ src/data/types.js
@@ -2,12 +2,12 @@ import Type from './model/Type';
 
 const types = [
 
-    { uid:'cosmic' },
-    { uid:'tech' },
-    { uid:'mutant' },
-    { uid:'skill' },
-    { uid:'science' },
-    { uid:'mystic' },
+    { uid: 'cosmic' },
+    { uid: 'tech' },
+    { uid: 'mutant' },
+    { uid: 'skill' },
+    { uid: 'science' },
+    { uid: 'mystic' },
 
 ].map((type) => new Type(type));
 
