1ed5d76244589307ab14bd4f28529f547b1c910d
Gabriel Hook <gabriel.hook@gmail.com>
2016-07-16 17:29:17 -0700

    show on rankup not max

--- src/view/Champion/ChampionUpgrades.jsx
+++ src/view/Champion/ChampionUpgrades.jsx
@@ -7,6 +7,11 @@ import lang from '../../service/lang';
 import m from 'mithril';
 /* eslint-enable no-unused-vars */
 
+const CATALYST_TYPE_BASIC = 'basic';
+const CATALYST_TYPE_CLASS = 'class';
+const CATALYST_TYPE_ALPHA = 'alpha';
+const CATALYST_TYPE_GOLD = 'gold';
+
 const ChampionUpgrades = {
     view(ctrl, { champions }) {
         const catalysts = champions
@@ -14,9 +19,7 @@ const ChampionUpgrades = {
             .filter(({ attr }) => {
                 const definitionStars = STAR_RANK_LEVEL[ attr.stars ];
                 const definitionRank = definitionStars[ attr.rank ];
-                const isMaxed = definitionStars.ranks === attr.rank && definitionRank.levels === attr.level;
-                const isRankUp = definitionStars.ranks > attr.rank && definitionRank.levels === attr.level;
-                return isMaxed || isRankUp;
+                return definitionStars.ranks > attr.rank && definitionRank.levels === attr.level;
             })
             // map upgrades needed
             .map(({ attr }) => ({
@@ -27,17 +30,17 @@ const ChampionUpgrades = {
             // add to the right category
             .reduce((collection, { catalysts, typeId, typeIndex }) => {
                 catalysts.forEach(({ type, tier, amount }) => {
-                    if(type === 'gold') {
-                        collection[ type ].amount += amount;
+                    if(type === CATALYST_TYPE_GOLD) {
+                        collection[ type ] += amount;
                     }
-                    else if(type === 'basic' || type === 'alpha') {
+                    else if(type === CATALYST_TYPE_BASIC || type === CATALYST_TYPE_ALPHA) {
                         collection[ type ].push({
                             type,
                             tier,
                             amount,
                         });
                     }
-                    else if (type === 'class') {
+                    else if (type === CATALYST_TYPE_CLASS) {
                         collection[ type ].push({
                             type,
                             tier,
@@ -48,18 +51,25 @@ const ChampionUpgrades = {
                     }
                 });
                 return collection;
-            }, { basic: [], class: [], alpha: [], gold: { type: 'gold', amount: 0 } });
+            }, {
+                [ CATALYST_TYPE_BASIC ]: [],
+                [ CATALYST_TYPE_CLASS ]: [],
+                [ CATALYST_TYPE_ALPHA ]: [],
+                [ CATALYST_TYPE_GOLD ]: 0,
+            });
 
         [ 'basic', 'class', 'alpha' ].forEach((type) => {
             catalysts[ type ] = catalysts[ type ]
                 // sort by tier ascending
-                .sort((type === 'class')? ({ tier: tA, typeIndex: tyA }, { tier: tB, typeIndex: tyB }) => {
-                    const compare = tA - tB;
-                    if(compare !== 0) {
-                        return compare;
-                    }
-                    return tyA - tyB;
-                }: ({ tier: tA }, { tier: tB }) => tA - tB)
+                .sort((type === CATALYST_TYPE_CLASS)?
+                    ({ tier: tA, typeIndex: tyA }, { tier: tB, typeIndex: tyB }) => {
+                        const compare = tA - tB;
+                        if(compare !== 0) {
+                            return compare;
+                        }
+                        return tyA - tyB;
+                    }: ({ tier: tA }, { tier: tB }) => tA - tB
+                )
                 // fold same tier/type into each other
                 .reduce((array, current, index) => {
                     if(index === 0) {
@@ -77,29 +87,32 @@ const ChampionUpgrades = {
                 }, []);
         });
 
-        return (!catalysts.gold.amount)? (
+        return (!catalysts.gold)? (
             <div m="ChampionUpgrades" />
         ): (
             <div
                 m="ChampionUpgrades"
                 title={ lang.get('upgrade-cost') }
                 class="champion-upgrade">
-                { [ 'basic', 'class', 'alpha' ].map((type) => {
-                    return catalysts[ type ].map(({ type, tier, amount, typeId }) => (
-                        <span class="champion-upgrade-catalyst">
-                            { amount } x
-                            <ImageIcon
-                                src={ (type === 'class')
-                                    ? `images/catalysts/tier_${ tier }_${ typeId }.png`
-                                    : `images/catalysts/tier_${ tier }_${ type }.png`
-                                }
-                                icon="share-alt"
-                            />
-                        </span>
-                    ));
-                }) }
+                { [ CATALYST_TYPE_BASIC, CATALYST_TYPE_CLASS, CATALYST_TYPE_ALPHA ]
+                    .filter((type) => Boolean(catalysts[ type ]))
+                    .map((type) => {
+                        return catalysts[ type ].map(({ type, tier, amount, typeId }) => (
+                            <span class="champion-upgrade-catalyst">
+                                { amount } x
+                                <ImageIcon
+                                    src={ (type === CATALYST_TYPE_CLASS)
+                                        ? `images/catalysts/tier_${ tier }_${ typeId }.png`
+                                        : `images/catalysts/tier_${ tier }_${ type }.png`
+                                    }
+                                    icon="share-alt"
+                                />
+                            </span>
+                        ));
+                    })
+                }
                 <span class="champion-upgrade-catalyst">
-                    { catalysts.gold.amount } x
+                    { catalysts[ CATALYST_TYPE_GOLD ] } x
                     <ImageIcon src={ 'images/catalysts/gold.png' } icon="cloud" />
                 </span>
             </div>
