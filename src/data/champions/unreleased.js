9505a403cbd6de0e3cac36fabe0403b6c1b65e69
hook <gabrielhook@gmail.com>
2016-08-17 16:00:32 -0700

    add assets and placeholders for karnak, quake, rogue

--- src/data/champions/unreleased.js
+++ src/data/champions/unreleased.js
@@ -2,8 +2,11 @@ import { CHAMPION } from '../model/Champion';
 
 export const UNRELEASED_CHAMPIONS = [
     // Cosmic
+    CHAMPION.KARNAK,
+    CHAMPION.QUAKE,
     // Tech
     // Mutant
+    CHAMPION.ROGUE,
     CHAMPION.WOLVERINEWEAPONX,
     // Skill
     CHAMPION.REDSKULL,
