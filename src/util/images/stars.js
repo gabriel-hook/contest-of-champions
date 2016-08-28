32b3268d9487fc5ba06c57fbea202b0f9c82a992
Gabriel Hook <gabriel.hook@gmail.com>
2016-08-28 11:45:25 -0700

    adjust stars blue

--- src/util/images/stars.js
+++ src/util/images/stars.js
@@ -10,7 +10,7 @@ const IMAGE_STAR = imageStar.clone()
 const IMAGE_STAR_AWAKENED = imageStar.clone()
     .transform(add('#113'))
     .transform(gamma(0.5))
-    .transform(multiply({ red: 0.95, green: 0.95, blue: 1 }))
+    .transform(multiply({ red: 0.90, green: 0.90, blue: 0.95 }))
     .toDataUrl();
 
 export { IMAGE_STAR, IMAGE_STAR_AWAKENED };
