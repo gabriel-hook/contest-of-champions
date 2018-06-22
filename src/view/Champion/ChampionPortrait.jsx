4b54e54891c660894261c984f918ce3a36148dc8
Luiz012 <luiz.felipe.silva.01@gmail.com>
2018-06-21 21:05:21 -0300

    Show Sig Level on Roster Page

--- src/view/Champion/ChampionPortrait.jsx
+++ src/view/Champion/ChampionPortrait.jsx
@@ -63,8 +63,7 @@ const ChampionPortrait = {
             }
             if(showPi && (pi || champion.pi)) {
             title.push(
-                    <div
-                        class={ classNames('title-field', 'title-field-pi', {
+                    <div class={ classNames('title-field', 'title-field-pi', {
                             'title-field-pi-custom': pi && pi > 0,
                         }, 'title-field-sig') }
                     >{ lang.number(pi || champion.pi * scalePi) + lang.string(` sig: ${awakened}`)}</div>
