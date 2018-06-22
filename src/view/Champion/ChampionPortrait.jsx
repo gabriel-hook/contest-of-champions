c897fc689a78bc91c2ad7ee25ecac04eef3b3b18
Luiz012 <luiz.felipe.silva.01@gmail.com>
2018-06-21 21:19:14 -0300

    Fixing indentation

--- src/view/Champion/ChampionPortrait.jsx
+++ src/view/Champion/ChampionPortrait.jsx
@@ -63,10 +63,9 @@ const ChampionPortrait = {
             }
             if(showPi && (pi || champion.pi)) {
               title.push(
-                    <div class={ classNames('title-field', 'title-field-pi', {
-                                'title-field-pi-custom': pi && pi > 0,
-                              }, 'title-field-sig') }
-                              >{ lang.number(pi || champion.pi * scalePi) + lang.string(` sig: ${awakened}`)}</div>
+                <div class={ classNames('title-field', 'title-field-pi', {
+                  'title-field-pi-custom': pi && pi > 0, }, 'title-field-sig') }
+                  >{ lang.number(pi || champion.pi * scalePi) + lang.string(` sig: ${awakened}`)}</div>
                 );
             }
             const name = lang.string(`champion-${ uid }-shortname`, null) || lang.string(`champion-${ uid }-name`);
